84f959a9eac1eecc6e515a391d2eba7f34843063
daring-array-6961 <ebrahimnery96@gmail.com>
2020-01-03 04:53:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ function getFirstSelector(selector) {
     // for (let i = 0; i < lis.length; i++) {
     //
     // }
-    return document.querySelector('div');
+    return document.querySelector(div);
 }
