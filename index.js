1b5d7c2c82a622068b284d7c8b6c18ec2cbfe0ee
daring-array-6961 <ebrahimnery96@gmail.com>
2020-01-03 04:52:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ function getFirstSelector(selector) {
     // for (let i = 0; i < lis.length; i++) {
     //
     // }
-    return const li2 = document.querySelector('ul.ranked-list li ul li');
+    return document.querySelector('ul.ranked-list li ul li');
 }
